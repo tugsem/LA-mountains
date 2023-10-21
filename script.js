47c6cf5d7fec3118e4e34afbcec06eeb78fb85aa
Tuğsem Saranlı <saranlitugsem@gmail.com>
2023-10-22 01:24:33 +0300

    Add mobile menu functionality

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const header= document.getElementsByTagName('header')[0];
 const links = document.querySelectorAll('.nav-link');
-const logo = document.querySelector('#logo-img');
+const logo = document.querySelector('.logo-img');
 const displayTitle = document.querySelector('.logotitle');
+const mobileMenu = document.querySelector('.mobile-nav');
+const menu = document.querySelector('.dk-nav');
 
 const scrollHeader = () => {
-  if (document.documentElement.scrollTop > 50 ) {
+  if (document.documentElement.scrollTop > 50 && !menu.classList.contains('mobile') ){
     header.classList.add('scroll-header');
     links.forEach(element => {
       element.classList.add('changeColor');
@@ -12,6 +14,7 @@ const scrollHeader = () => {
     });
     logo.classList.add('addBorder');
     displayTitle.style.display = 'flex';
+    mobileMenu.style.color = '#4d4949';
   } else {
     header.classList.remove('scroll-header');
     links.forEach(element => {
@@ -19,6 +22,24 @@ const scrollHeader = () => {
     });
     logo.classList.remove('addBorder');
     displayTitle.style.display = 'none';
+    mobileMenu.style.color = '#fff';
   }
 }
+
+mobileMenu.addEventListener('click', () => {
+  menu.classList.toggle('mobile');
+  if (menu.classList.value === 'dk-nav mobile'){
+    mobileMenu.innerHTML = '&#10006;'
+  }else {
+    mobileMenu.innerHTML = '&#9776;'
+  }
+})
+
+links.forEach((link) => {
+  link.addEventListener('click', () => {
+    menu.classList.remove('mobile');
+    mobileMenu.innerHTML = '&#9776;'
+  })
+})
+
 window.onscroll = () => scrollHeader();
